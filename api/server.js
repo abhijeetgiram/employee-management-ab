cbf545013526045c42a26a90e03f00d88b5ef412
Abhijeet Giram <abhijeetgrm@gmail.com>
2025-08-09 15:49:03 +0530

    feat: Updated server js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(process.cwd(), "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+if (process.env.VERCEL) {
+  // On Vercel → export handler
+  module.exports = server;
+} else {
+  // Local dev
+  const port = 3000;
+  server.listen(port, () => {
+    console.log(`JSON Server running on http://localhost:${port}`);
+  });
+}
